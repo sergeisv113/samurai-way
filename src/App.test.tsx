f87687f2d52b48633d0466b5f30c4cc220202b95
Sergey <sergeisv113@gmail.com>
2023-04-14 15:19:45 +0300

    lesson-100

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from "react-dom";
-import SamApp from "./App";
-
-
-it('renders without crashing', () => {
-    const div = document.createElement('div')
-    ReactDOM.render(<SamApp/>, div)
-    ReactDOM.unmountComponentAtNode(div)
-})
--- src/components/Ava/utils/typeguards.ts
+++
@@ -1,3 +0,0 @@
-import { GithubUser } from '../../../types/userGit';
-
-export const isGithubUser = (userGit: any): userGit is GithubUser => 'id' in userGit;
